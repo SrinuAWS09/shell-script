4adf2499cbc14ea2413f3f44e225863948a7b61c
Srinu <gollavillisrinu09@gmail.com>
2024-12-31 12:37:03 +0530

    5th script is added

---
+++ 05_adding_two_numbers.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+NUMBER=$1
+NUMBER=$2
+TIMESTAMP=${date}
+echo "time at started is : ${TIMESTAMP}"
+echo " Given two number are: ${NUMBER1} and ${NUMBER2}"
+sum=${[$NUMBER1+$NUMBER2)}
+echo "sum of two number is: ${SUM}"
