c5c0cf5732aa51edab44f4619da2cd0565b3ac1c
Srinu <gollavillisrinu09@gmail.com>
2025-01-01 21:09:37 +0530

    7th script

---
+++ 07_Special_Variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "total variables passed: $@"
+echo "No of variables: $#"
+echo "script_name:$0"
+echo "present working directory:$PWD"
+echo "Current user home directory: $HOME"
+echo "Current user:$USER"
+echo "current process id :$$"
+echo "previous process id: "$!"
