1d57d1fd9aac1439a1b228f84a7e24ba31a7aa64
Srinu <gollavillisrinu09@gmail.com>
2025-01-03 16:45:56 +0530

    8th script

--- 09_Install_using_shell.sh
+++ 09_Install_using_shell.sh
@@ -7,4 +7,22 @@ then
     echo "ERROR: This is not root user"
     exit 1
 fi
+dnf install mysql -y
+if [ $? -ne 0 ]
+ then
+     echo "ERROR: Installation is failed...MYSQL"
+     exit 1
+else 
+     echo " Installation success.. MYSQL"
+fi
 dnf install git -y
+
+if [ $? -ne 0 ]
+ then
+     echo "ERROR: Installation is failed...GIT"
+     exit 1
+else 
+     echo " Installation success.. GIT"
+fi
+
+
