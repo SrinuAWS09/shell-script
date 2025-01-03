919acdd30ed9064f6e3109d086caef02915ebb4a
Srinu <gollavillisrinu09@gmail.com>
2025-01-03 15:53:34 +0530

    8th script

--- 08_Greater.sh
+++ 08_Greater.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 NUMBER=$1
-if [$NUMBER -gt 100]
-then
-echo " Given number is greater than 100"
+
+if [ $NUMBER -gt 100 ]
+then   
+    echo " Given number is greater than 100"
 else
-echo " Give number is smaller than 100"
+    echo " Give number is smaller than 100"
 fi
 
