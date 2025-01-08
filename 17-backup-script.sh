35b6db714969d1896cd07bb7cf75ce21aad8c449
Srinu <gollavillisrinu09@gmail.com>
2025-01-08 21:58:20 +0530

    17th backup-script.sh

--- 17-backup-script.sh
+++ 17-backup-script.sh
@@ -9,12 +9,19 @@ SOURCE_DIR=$1
 DEST_DIR=$2
 DAYS=${3:-14}  # If user is not providing number of days, we are taking 14 as default
 
+LOGS_FOLDER="/home/ec2-user/shellscript-logs"
+LOG_FILE=$(echo $0 | awk -F "/" '{print $NF}' | cut -d "." -f1 )
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
+LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
+
 
 
 USAGE(){
 echo -e "$R USAGE::$N sh 17-backup-script.sh <SOURCE_DIR> <DEST_DIR> <DAYS(Optional)>"
 
 }
+
+mkdir -p /home/ec2-user/shellscript-logs
 
 if [ $# -lt 2  ]
 then
